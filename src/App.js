3e1df1fa1e57f7b7fc119783d00ea26e42fef267
rachel490 <ccori12@naver.com>
2022-11-07 22:11:05 +0900

    add function for getting user input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
-	play() {
+	async play() {
 		console.log('숫자 야구 게임을 시작합니다.');
 		const computerNum = this.chooseTargetNumber();
-		
-    console.log('computerNum: ',computerNum)
+		const userInput = await this.getUserInput('숫자를 입력해주세요 : ');
+
+		console.log('computerNum: ', computerNum);
+		console.log('userInput: ', userInput);
 	}
 
 	chooseTargetNumber() {
 		const targetNum = MissionUtils.Random.pickNumberInRange(100, 999);
 		return targetNum;
+	}
+
+	getUserInput(query) {
+		const readline = require('readline');
+		const rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout,
+		});
+
+		return new Promise(resolve =>
+			rl.question(query, ans => {
+				rl.close();
+				resolve(ans);
+			}),
+		);
 	}
 }
 
