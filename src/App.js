f1c1cf67bb7b76195eb2c7578542e767975b2512
rachel490 <ccori12@naver.com>
2022-11-07 22:08:10 +0900

    add function for targeting random number

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+	play() {
+		console.log('숫자 야구 게임을 시작합니다.');
+		const computerNum = this.chooseTargetNumber();
+		
+    console.log('computerNum: ',computerNum)
+	}
+
+	chooseTargetNumber() {
+		const targetNum = MissionUtils.Random.pickNumberInRange(100, 999);
+		return targetNum;
+	}
 }
 
 module.exports = App;
