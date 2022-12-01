1a4d1d531d0a5aa0e0787ff9da89e5410c47571c
Minhye Kang <alsgp9898@naver.com>
2022-12-01 17:10:11 +0900

    feat: 중복된 숫자가 있는 경우 에러 발생

--- src/InputView.js
+++ src/InputView.js
@@ -8,6 +8,7 @@ const ERROR = {
   THREE_DIGITS: 3,
   NOT_THREE_DIGITS: "3자리 숫자를 입력해주세요.",
   NOT_NUMBER: "숫자를 입력해주세요",
+  DUPLICATE: "중복된 숫자가 있습니다."
 };
 
 const InputView = {
@@ -22,6 +23,7 @@ const ErrorHandling = {
   userNumber(input) {
     if (input.length !== ERROR.THREE_DIGITS) throw new Error(ERROR.NOT_THREE_DIGITS);
     if (isNaN(input)) throw new Error(ERROR.NOT_NUMBER);
+    if (new Set([...input]).size !== ERROR.THREE_DIGITS) throw new Error(ERROR.DUPLICATE);
   },
 };
 
