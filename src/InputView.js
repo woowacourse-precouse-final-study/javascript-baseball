b74d17baf28ff27d52e867796e90d79def63b224
Minhye Kang <alsgp9898@naver.com>
2022-12-01 16:49:45 +0900

    feat: 입력된 숫자가 3자리가 아닌 경우 에러 발생

--- src/InputView.js
+++ src/InputView.js
@@ -4,9 +4,23 @@ const INPUT_QUERY = {
   NUMBER: "숫자를 입력해주세요 : ",
 };
 
+const ERROR_MSG = {
+  THREE_DIGITS: "3자리 숫자를 입력해주세요.",
+};
+
 const InputView = {
   readUserNumber() {
-    Console.readLine(INPUT_QUERY.NUMBER, (input) => {});
+    Console.readLine(INPUT_QUERY.NUMBER, (input) => {
+      ErrorHandling.userNumber(input);
+    });
+  },
+};
+
+const ErrorHandling = {
+  userNumber(input) {
+    if (input.length !== 3) {
+      throw new Error(ERROR_MSG.THREE_DIGITS);
+    }
   },
 };
 
