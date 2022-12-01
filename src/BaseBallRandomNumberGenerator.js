89c7466b992c858eb3f08f36a9b6c9c24e74fad9
murpin <qw04011@gmail.com>
2022-12-01 21:29:37 +0900

    refactor(BaseBallGame): 숫자야구 게임 숫자 생성 상수 적용

--- src/BaseBallRandomNumberGenerator.js
+++ src/BaseBallRandomNumberGenerator.js
@@ -1,9 +1,10 @@
 const { Random } = require('@woowacourse/mission-utils');
+const { BASEBALL_GAME_OPTION } = require('./constant/BaseBallGame');
 
 function BaseBallRandomNumberGenerator() {
   const computer = [];
-  while (computer.length < 3) {
-    const number = Random.pickNumberInRange(1, 9);
+  while (computer.length < BASEBALL_GAME_OPTION.number_len) {
+    const number = Random.pickNumberInRange(BASEBALL_GAME_OPTION.min_number, BASEBALL_GAME_OPTION.max_number);
     if (!computer.includes(number)) {
       computer.push(number);
     }
