0a187f35401be210da5bee0f59b769ae64289a79
minsun <39366835+menduck@users.noreply.github.com>
2022-12-03 20:20:06 +0900

    Revert "[숫자 야구 게임] 김민선 미션 제출합니다."

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-const Controller = require('../src/Controller/Controller');
 class App {
-  constructor() {
-    this.controller = new Controller();
-  }
-  
-  play() {
-    this.controller.Hello();
-  }
+  play() {}
 }
-
-const app = new App();
-app.play()
 
 module.exports = App;
