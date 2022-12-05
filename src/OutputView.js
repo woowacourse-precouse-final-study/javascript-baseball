f5dfafaf6117bc94be56d846339812ed35d744b2
Minhye Kang <alsgp9898@naver.com>
2022-12-05 14:21:12 +0900

    refactor: trim()을 이용하여 출력 시 공백을 제거하도록 변경

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,13 +1,13 @@
-const {Console} = require("@woowacourse/mission-utils");
+const { Console } = require("@woowacourse/mission-utils");
 
 const OUTPUT = {
   GAME_START: "숫자 야구 게임을 시작합니다.",
   ANSWER: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
   NOTHING: "낫싱",
-  BLANK: "",
-  BALL: "볼 ",
-  STRIKE: "스트라이크"
-}
+  NULL: "",
+  BALL: "볼",
+  STRIKE: "스트라이크",
+};
 
 const OutputView = {
   startGame() {
@@ -17,15 +17,15 @@ const OutputView = {
   printGameResult(ball, strike) {
     if (!strike && !ball) return Console.print(OUTPUT.NOTHING);
     return Console.print(
-      `${ball ? ball + OUTPUT.BALL : OUTPUT.BLANK}${
-        strike ? strike + OUTPUT.STRIKE : OUTPUT.BLANK
-      }`
+      `${ball ? ball + OUTPUT.BALL : OUTPUT.NULL} ${
+        strike ? strike + OUTPUT.STRIKE : OUTPUT.NULL
+      }`.trim()
     );
   },
 
   printAnswerMessage() {
-    Console.print(OUTPUT.ANSWER)
-  }
-}
+    Console.print(OUTPUT.ANSWER);
+  },
+};
 
 module.exports = OutputView;
