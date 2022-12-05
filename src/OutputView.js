13777161149ad4954e7dc9662b54d1455617c508
Minhye Kang <alsgp9898@naver.com>
2022-12-05 16:48:41 +0900

    refactor: 상수 객체 내의 대문자 소문자로 변경

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,30 +1,30 @@
 const { Console } = require("@woowacourse/mission-utils");
 
 const OUTPUT = {
-  GAME_START: "숫자 야구 게임을 시작합니다.",
-  ANSWER: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-  NOTHING: "낫싱",
-  NULL: "",
-  BALL: "볼",
-  STRIKE: "스트라이크",
+  game_start: "숫자 야구 게임을 시작합니다.",
+  answer: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  nothing: "낫싱",
+  null: "",
+  ball: "볼",
+  strike: "스트라이크",
 };
 
 const OutputView = {
   startGame() {
-    Console.print(OUTPUT.GAME_START);
+    Console.print(OUTPUT.game_start);
   },
 
   printGameResult(ball, strike) {
-    if (!strike && !ball) return Console.print(OUTPUT.NOTHING);
+    if (!strike && !ball) return Console.print(OUTPUT.nothing);
     return Console.print(
-      `${ball ? ball + OUTPUT.BALL : OUTPUT.NULL} ${
-        strike ? strike + OUTPUT.STRIKE : OUTPUT.NULL
+      `${ball ? ball + OUTPUT.ball : OUTPUT.null} ${
+        strike ? strike + OUTPUT.strike : OUTPUT.null
       }`.trim()
     );
   },
 
   printAnswerMessage() {
-    Console.print(OUTPUT.ANSWER);
+    Console.print(OUTPUT.answer);
   },
 };
 
